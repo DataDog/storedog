55924477952c94bd97d715d2c6da609ac1bbba9f
Colin Cole <colin.cole@datadoghq.com>
2022-11-03 17:09:23 -0400

    attempt to serve static resource

--- services/ads/java/src/main/java/adsjava/AdsJavaApplication.java
+++ services/ads/java/src/main/java/adsjava/AdsJavaApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.util.StreamUtils;
 
 @SpringBootApplication
 @RestController
