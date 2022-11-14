8b75813eed0b7e2f003e48c66d832ca4d5f02e4c
Colin Cole <colin.cole@datadoghq.com>
2022-11-14 14:41:02 -0500

    add mvp java ad service

--- services/ads/java/src/main/java/adsjava/AdsJavaApplication.java
+++ services/ads/java/src/main/java/adsjava/AdsJavaApplication.java
@@ -6,30 +6,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.MediaType;
 import org.springframework.util.StreamUtils;
+import java.io.*;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.apache.commons.io.IOUtils;
+import java.util.concurrent.ThreadLocalRandom;
 
 @SpringBootApplication
 @RestController
 public class AdsJavaApplication {
 
-  @RequestMapping(value = "/Image/{id:.+}", method = RequestMethod.GET)
-  public ResponseEntity<byte[]> getImage(@PathVariable("id") String id) {
-    byte[] image = imageService.getImage(id);
-    return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image);
+  @RequestMapping(
+    value = "/banner",
+    produces = MediaType.IMAGE_JPEG_VALUE
+  )
+  public @ResponseBody byte[] getImageWithMediaType() throws IOException {
+    int randomNum = ThreadLocalRandom.current().nextInt(1, 3 + 1);
+    String imagePath = "/static/ad" + randomNum + ".jpg";
+    InputStream in = getClass()
+        .getResourceAsStream(imagePath);
+    return IOUtils.toByteArray(in);
   }
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
-	}
-
-	@RequestMapping("/banner")
-	public String banner() {
-		return "Hello Docker World 2";
-	}
-
-	@RequestMapping("/test")
-	public String test() {
-		return "Test";
+		return "Hello from Advertisements (Java)";
 	}
 
 	public static void main(String[] args) {
