8fa148712bd3f797c001743b0fa9a2d523101ac4
Devin Ford <devin.ford@datadoghq.com>
2023-04-05 17:18:44 -0400

    feat: clean up shell file

--- services/dbm/dbm_exec.sh
+++ services/dbm/dbm_exec.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-exec postgres -c config_file=/etc/postgresql/13/main/postgresql.conf
-
-sleep(20)
-
-exec psql -U postgres -d postgres -a -f /etc/postgresql/13/main/dbm_setup.sql
+docker-compose exec postgres psql -U postgres -d postgres -a -f /etc/postgresql/13/main/dbm_setup.sql
