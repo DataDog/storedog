381a65ff8819ca06df8137aa1dd11c7e01e8939d
Alex Rosenkranz <alex.rosenkranz@datadoghq.com>
2024-01-31 14:54:00 +0000

    adds initial dbm setup

--- services/backend/db/restore/restore.sql
+++ services/backend/db/restore/restore.sql
@@ -1,6 +1,10 @@
-create user datadog with password 'datadog';
-grant pg_monitor to datadog;
-grant SELECT ON pg_stat_database to datadog;
+CREATE user datadog WITH password 'datadog';
+CREATE SCHEMA datadog;
+GRANT USAGE ON SCHEMA datadog TO datadog;
+GRANT USAGE ON SCHEMA public TO datadog;
+GRANT pg_monitor TO datadog;
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+GRANT SELECT ON pg_stat_database TO datadog;
 
 --
 -- PostgreSQL database dump
@@ -45,6 +49,29 @@ SET row_security = off;
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+/* FOR DATADOG DBM */
+CREATE OR REPLACE FUNCTION datadog.explain_statement(
+   l_query TEXT,
+   OUT explain JSON
+)
+RETURNS SETOF JSON AS
+$$
+DECLARE
+curs REFCURSOR;
+plan JSON;
+
+BEGIN
+   OPEN curs FOR EXECUTE pg_catalog.concat('EXPLAIN (FORMAT JSON) ', l_query);
+   FETCH curs INTO plan;
+   CLOSE curs;
+   RETURN QUERY SELECT plan;
+END;
+$$
+LANGUAGE 'plpgsql'
+RETURNS NULL ON NULL INPUT
+SECURITY DEFINER;
+/* END DBM */
 
 --
 -- Name: action_mailbox_inbound_emails; Type: TABLE; Schema: public; Owner: postgres
