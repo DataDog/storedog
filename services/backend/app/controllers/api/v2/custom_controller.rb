eb3d1226d5576bd8573b156375c30b46d582c247
guilhermevrs <guilherme.vieiraschwade@datadoghq.com>
2025-02-19 23:38:16 +0000

    feat: add a faster image with more delay in trace

--- services/backend/app/controllers/api/v2/custom_controller.rb
+++ services/backend/app/controllers/api/v2/custom_controller.rb
@@ -2,10 +2,12 @@ module Api
     module V2
       class CustomController < ApplicationController
         def about_us
-          # Sleep for 0.6 seconds to introduce a delay before responding
-          sleep 0.6
+          # Sleep for 3.1 seconds to introduce a delay before responding
+          sleep 3.1
+          # Set the custom header
+          response.set_header('Timing-Allow-Origin', '*')
 
-          render json: {image: "https://storage.googleapis.com/shopist-io-large-image-for-rum-lcp/high-res-kitchen-table.jpg"}, status: :ok
+          render json: {image: "/assets/t-shirt-0.png"}, status: :ok
         end
       end
     end
