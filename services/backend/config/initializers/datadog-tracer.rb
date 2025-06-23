ea0de72ac3af73b5bc4356abebea869604a0ee3e
Alex Rosenkranz <alex.rosenkranz@datadoghq.com>
2025-06-23 23:03:50 +0000

    renames nginx, adds backend log, renames ads

--- services/backend/config/initializers/datadog-tracer.rb
+++ services/backend/config/initializers/datadog-tracer.rb
@@ -3,16 +3,16 @@ if ENV['WORKER'] == 'true'
     c.service = ENV['DD_SERVICE'] || 'store-worker'
     # Activates and configures an integration
     c.tracing.instrument :sidekiq, tag_args: true
-    c.tracing.instrument :pg, service_name: 'postgres'
-    c.tracing.instrument :active_support, cache_service: 'store-worker-cache'
-    c.tracing.instrument :redis, service_name: 'store-worker-redis'
+    c.tracing.instrument :pg
+    c.tracing.instrument :active_support
+    c.tracing.instrument :redis
   end
 else
   Datadog.configure do |c|
     c.service = ENV['DD_SERVICE'] || 'store-backend'
     # Activates and configures an integration
-    c.tracing.instrument :pg, service_name: 'postgres'
-    c.tracing.instrument :dalli, service_name: 'store-backend-memcached'
-    c.tracing.instrument :active_support, cache_service: 'store-backend-cache'
+    c.tracing.instrument :pg
+    c.tracing.instrument :active_support
+    c.tracing.instrument :redis
   end
 end
