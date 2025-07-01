d4ec512d36e748b9a54e4ffe799fdc40a51563a4
Shawn Potts <shawn.potts@datadoghq.com>
2025-07-01 10:52:16 +0100

    adding AB testing to nginx config

--- services/nginx/docker-entrypoint.sh
+++ services/nginx/docker-entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 # Use 127.0.0.11 as the default resolver for Docker Compose
 export NGINX_RESOLVER=${NGINX_RESOLVER:-127.0.0.11}
 
-# Substitute the variable in the template and output the final config
-envsubst '$NGINX_RESOLVER' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+# Substitute all relevant variables in the template and output the final config
+envsubst '$NGINX_RESOLVER $ADS_A_UPSTREAM $ADS_B_UPSTREAM $ADS_B_PERCENT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start NGINX in the foreground
 exec nginx -g 'daemon off;'
