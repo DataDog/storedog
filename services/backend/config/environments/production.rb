ee6c984d8e0f76ff4280a7cf2186f86e5cdc24bd
Shawn Potts <shawn.potts@datadoghq.com>
2026-01-20 14:06:08 +0000

    fixing assetView template error in worker

--- services/backend/config/environments/production.rb
+++ services/backend/config/environments/production.rb
@@ -34,7 +34,9 @@ Rails.application.configure do
   config.assets.precompile += %w[ spree/backend/all.js spree/backend/all.css ]
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # Assets are precompiled during Docker build - disable runtime compilation
+  # to prevent ExecJS errors in Sidekiq workers
+  config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
